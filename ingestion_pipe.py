cc7f3401b8881e37652bd8f127f9764a15d9ae43
aby <olayodepossible@gmail.com>
2026-03-06 21:08:59 +0100

    implement doc loader function

--- ingestion_pipe.py
+++ ingestion_pipe.py
@@ -10,3 +10,15 @@ from langchain_huggingface import HuggingFaceEmbeddings
 import gradio as gr
 
 load_dotenv(override=True)
+
+def load_documents(directory):
+    f""""Load documents from {directory} directory."""
+    print(f"Loading documents from {directory} directory...")
+
+    #check if the directory exists
+    if not os.path.exists(directory):
+        raise FileNotFoundError(f"Directory {directory} does not exist.")
+    
+    loader = DirectoryLoader(directory, glob="**/*.txt",  loader_cls=TextLoader, loader_kwargs={'encoding': 'utf-8'})
+    documents = loader.load()
+    return documents
